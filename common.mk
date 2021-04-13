0378fd10c7396fac40028d49beabb58936c53ea5
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-04-13 11:45:11 -0700

    sdm845-common: Enable built-in Google Camera
    Using firebird11's inline implementation, and Arnova82's port of GCam 8.1
    (`Snap_GCam_6.210329.0153build-8.1.101.apk` build, org.codeaurora.snapcam package name).
    
    Recommend going into Settings > Advanced > Stream Config, toggling on `Enable OPMode`,
    then setting everything for both front & back main sensors to `OpMode (Default)`.

--- common.mk
+++ common.mk
@@ -106,6 +106,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Google Camera
+$(call inherit-product, packages/apps/GoogleCamera/config.mk)
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
