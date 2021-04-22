4795a03dbf7af72122ac0d1c318f92ef248886fa
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-04-22 07:58:38 -0700

    sdm845-common: livedisplay: Update DisplayModes with OOS Q nodes
    Updating livedisplay DisplayModes to use OOS Q style kernel nodes, like what sm8150 & sm8250 utilize
    (and what other sdm845 kernels use that merged all of the OOS Q updates instead of a small handful).
    Fix up some paths & sepolicy stuff while in here to get it consistent.

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -23,6 +23,12 @@
 #include "AntiFlicker.h"
 #include "DisplayModes.h"
 
+using android::OK;
+using android::sp;
+using android::status_t;
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
 using ::vendor::lineage::livedisplay::V2_1::IAntiFlicker;
 using ::vendor::lineage::livedisplay::V2_1::IDisplayModes;
 using ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment;
@@ -32,32 +38,49 @@ using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
 
 int main() {
+    status_t status = OK;
+
+    android::ProcessState::initWithDriver("/dev/vndbinder");
+
+    LOG(INFO) << "LiveDisplay HAL service is starting.";
+
     std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
-    android::sp<IAntiFlicker> afService = new AntiFlicker();
-    android::sp<IDisplayModes> modesService = new DisplayModes();
-    android::sp<IPictureAdjustment> paService = new PictureAdjustment(controller);
+    sp<AntiFlicker> antiFlicker = new AntiFlicker();
+    sp<DisplayModes> dm = new DisplayModes();
+    sp<PictureAdjustment> pa = new PictureAdjustment(controller);
 
-    android::hardware::configureRpcThreadpool(2, true /*callerWillJoin*/);
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    if (afService->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register anti flicker HAL service.";
-        return 1;
+    // AntiFlicker service
+    status = antiFlicker->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Could not register service for LiveDisplay HAL AntiFlicker Iface ("
+                   << status << ")";
+        goto shutdown;
     }
 
-    if (modesService->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register display modes HAL service.";
-        return 1;
+    // DisplayModes service
+    status = dm->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Could not register service for LiveDisplay HAL DisplayModes Iface ("
+                   << status << ")";
+        goto shutdown;
     }
 
-    if (paService->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register picture adjustment HAL service.";
-        return 1;
+    // PictureAdjustment service
+    status = pa->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Could not register service for LiveDisplay HAL PictureAdjustment Iface ("
+                   << status << ")";
+        goto shutdown;
     }
 
     LOG(INFO) << "LiveDisplay HAL service ready.";
-
     android::hardware::joinRpcThreadpool();
+    // Should not pass this line
 
-    LOG(ERROR) << "LiveDisplay HAL service failed to join thread pool.";
+shutdown:
+    // In normal operation, we don't expect the thread pool to shutdown
+    LOG(ERROR) << "LiveDisplay HAL service is shutting down.";
     return 1;
 }
